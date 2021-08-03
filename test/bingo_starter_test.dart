55b46d24740166e803bc5214fba8b95592fe91c4
illusion <dhruvtiwari110058@gmail.com>
2021-08-03 15:43:00 +0530

    commented tests

--- test/bingo_starter_test.dart
+++ test/bingo_starter_test.dart
@@ -1,8 +1,8 @@
 // Package imports:
-import 'package:flutter_test/flutter_test.dart';
+// import 'package:flutter_test/flutter_test.dart';
 
 // Project imports:
-import 'package:bingo_starter/bingo_starter.dart';
+// import 'package:bingo_starter/bingo_starter.dart';
 
 void main() {
   // test('adds one to input values', () {
